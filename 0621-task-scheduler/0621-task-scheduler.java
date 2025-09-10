f9d001cc39ce6731c63381443082fdd0c7614afa
Ani <111388269+Aniruddh1210@users.noreply.github.com>
2025-09-10 16:38:39 +0530

    Time: 31 ms (24.28%), Space: 44.9 MB (99.34%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,47 +1,39 @@
-// User function Template for Java
-
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(char c : tasks){
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
+        if (tasks.length == 0) return 0;
 
-        PriorityQueue<Integer> maxheap = new PriorityQueue<>((a,b)->b-a);
-        for(int freq : map.values()){
-            maxheap.add(freq);
-        }
+        // frequency map
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : tasks) map.put(c, map.getOrDefault(c, 0) + 1);
+
+        // max heap of frequencies
+        PriorityQueue<Integer> maxheap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int f : map.values()) maxheap.add(f);
 
         int time = 0;
-        ArrayList<Integer> temp = new ArrayList<>();
 
-        while(!maxheap.isEmpty()){
-            for(int i=0;i<=n;i++){
-                if(maxheap.isEmpty()){
-                    // put remaining tasks back
-                    for(int j=0;j<temp.size();j++){
-                        if(temp.get(j) > 0){
-                            maxheap.add(temp.get(j));
-                        }
-                    }
-                    // only add idle time if heap still has tasks
-                    if(!maxheap.isEmpty() && temp.size() <= n){
-                        time += (n + 1 - temp.size());
-                    }
+        while (!maxheap.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            int tasksExecuted = 0;
+
+            // try to execute up to (n+1) tasks in this cycle
+            for (int i = 0; i <= n; i++) {
+                if (!maxheap.isEmpty()) {
+                    int cnt = maxheap.poll();
+                    tasksExecuted++;
+                    if (cnt - 1 > 0) temp.add(cnt - 1);
+                } else {
                     break;
                 }
-
-                int curr = maxheap.poll();
-                temp.add(curr - 1);
-                time++;
             }
 
-            for(int j=0;j<temp.size();j++){
-                if(temp.get(j) > 0){
-                    maxheap.add(temp.get(j));
-                }
-            }
-            temp.clear();
+            // put remaining counts back
+            for (int v : temp) maxheap.add(v);
+
+            // if heap is empty now, this was the last (partial) cycle: add only tasksExecuted
+            // otherwise we used (n+1) time units for this full cycle
+            if (maxheap.isEmpty()) time += tasksExecuted;
+            else time += (n + 1);
         }
 
         return time;
