3ddadc73d15c1686c386d819500cc1753035b344
Ani <111388269+Aniruddh1210@users.noreply.github.com>
2026-01-04 16:40:07 +0530

    Time: 1 ms (86.37%), Space: 47.1 MB (75.76%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,10 +1,10 @@
 class Solution {
 
-    private void dfs(int[][] grid,int[] visited,int node){
+    private void dfs(int[][] grid, int[] visited, int node) {
         visited[node] = 1;
-        for(int i=0;i<grid[0].length;i++){
-            if(grid[node][i]==1 && i!=node && visited[i]==0){
-                dfs(grid,visited,i);
+        for (int i = 0; i < grid[0].length; i++) {
+            if (grid[node][i] == 1 && i != node && visited[i] == 0) {
+                dfs(grid, visited, i);
             }
         }
     }
@@ -13,13 +13,13 @@ class Solution {
         int[] visited = new int[isConnected.length];
         int count = 0;
 
-        for(int i=0;i<isConnected.length;i++){
-            for(int j=0;j<isConnected[0].length;j++){
-                if(visited[i]==0){
-                    count++;
-                    dfs(isConnected,visited,i);
-                }
+        for (int i = 0; i < isConnected.length; i++) {
+
+            if (visited[i] == 0) {
+                count++;
+                dfs(isConnected, visited, i);
             }
+
         }
 
         return count;
