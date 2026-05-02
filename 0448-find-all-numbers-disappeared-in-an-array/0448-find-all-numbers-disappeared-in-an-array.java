ac0e0e070be9a3ae53e3f614c6b4d28d0ffe00ac
Ani <111388269+Aniruddh1210@users.noreply.github.com>
2026-05-03 03:35:42 +0530

    Time: 6 ms (83.89%), Space: 67.1 MB (62.18%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,18 +1,42 @@
+//optimal soln, we mark the visited array indexes negative and then we only return the positive ones
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> ans = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
 
-        for(int i :nums){
-            set.add(i);
+        for (int i = 0; i < nums.length; i++) {
+            int val = Math.abs(nums[i]) - 1;
+
+            if (nums[val] > 0) {
+                nums[val] = -nums[val];
+            }
         }
 
-        for(int i=1;i<=nums.length;i++){
-            if(!set.contains(i)){
-                ans.add(i);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                ans.add(i + 1);
             }
         }
 
         return ans;
     }
 }
+
+//hashmap soln
+// class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+//         List<Integer> ans = new ArrayList<>();
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for(int i :nums){
+//             set.add(i);
+//         }
+
+//         for(int i=1;i<=nums.length;i++){
+//             if(!set.contains(i)){
+//                 ans.add(i);
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
