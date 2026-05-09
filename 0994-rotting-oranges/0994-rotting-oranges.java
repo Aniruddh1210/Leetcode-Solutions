54ad91edf0ef576ace18db2b2d55806c792b520e
Ani <111388269+Aniruddh1210@users.noreply.github.com>
2026-05-10 03:15:55 +0530

    Time: 2 ms (90.47%), Space: 44.9 MB (5.45%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,50 +1,66 @@
-import java.util.*;
-
 class Solution {
-    public int orangesRotting(int[][] grid) {
 
-        Queue<int[]> q = new LinkedList<>();
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==2){
-                q.add(new int[]{i,j,0});
-                }
-            }
-        }
+    public static int bfs(int[][]grid,Queue<int[]>q){
 
         int maxtime = 0;
 
-        int n = grid.length;
-        int m = grid[0].length;
-
         while(!q.isEmpty()){
+        
+        int[] vals = q.poll();
+        int row = vals[0];
+        int col = vals[1];
+        int time = vals[2];
 
-            int[] curr = q.poll();
-            int row = curr[0];
-            int col = curr[1];
-            int time = curr[2];
+        maxtime = Math.max(maxtime,time);
 
-            maxtime = Math.max(time,maxtime);
 
-            int[] delRow = {-1,0,1,0};
-            int[] delCol = {0,+1,0,-1};
+        int[] delrow = {-1,0,1,0};
+        int[] delcol = {0,1,0,-1};
 
-            for(int i=0;i<4;i++){
-                int nrow = row + delRow[i];
-                int ncol = col + delCol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m) {
-                    if(grid[nrow][ncol]==1){
-                        grid[nrow][ncol]=2;
-                        q.add(new int[]{nrow,ncol,time+1});
-                    }
+
+        for(int i=0;i<4;i++){
+
+            int newrow = row + delrow[i];
+            int newcol = col + delcol[i];
+
+            if(newrow>=0 && newrow<grid.length && newcol>=0 && newcol<grid[0].length){
+                if(grid[newrow][newcol]==1){
+                    grid[newrow][newcol]=2;
+                    q.add(new int[]{newrow,newcol,time+1});
                 }
             }
         }
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1) return -1;
-            }
         }
         return maxtime;
+
+    }
+
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
+
+        int flag = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 2)
+                    q.add(new int[] { i, j, 0 });
+            }
+        }
+
+        if (q.isEmpty()) {
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++)
+                if (grid[i][j] == 1) return -1;
+                return 0; // ✅ no rotten, no fresh → 0
+        }   
+
+        int time = bfs(grid, q);
+
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++)
+                if (grid[i][j] == 1)
+                    return -1;
+
+        return time;
     }
 }
