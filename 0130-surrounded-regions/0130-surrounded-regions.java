cf2531c75b55207eaebad7bfcbfdb59ed16baf10
Ani <111388269+Aniruddh1210@users.noreply.github.com>
2026-05-19 00:38:36 +0530

    Time: 2 ms (75.79%), Space: 47.6 MB (85.12%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,30 +1,15 @@
-//just mark the boundary O's and all its neighbours, don't change those O's change all the rest
-//can use either bfs or dfs
-
 class Solution {
 
-    public static void bfs(int r, int c, char[][] board, int[][] vis) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] { r, c });
-        vis[r][c] = 1;
-
-        int[] delrow = { 0, 1, 0, -1 };
-        int[] delcol = { 1, 0, -1, 0 };
-
-        while (!q.isEmpty()) {
-            int val[] = q.poll();
-            int row = val[0];
-            int col = val[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-
-                if (nrow >= 0 && nrow < board.length && ncol >= 0 && ncol < board[0].length) {
-                    if(board[nrow][ncol]=='O' && vis[nrow][ncol]==0){
-                        vis[nrow][ncol]=1;
-                        q.add(new int[]{nrow,ncol});
-                    }
+    public static void dfs(char[][]board,int[][]vis,int r, int c){
+        int[] delrow = {0,1,0,-1};
+        int[] delcol = {1,0,-1,0};
+        for(int i=0;i<4;i++){
+            int nr = r + delrow[i];
+            int nc = c + delcol[i];
+            if(nr>=0 && nc>=0 && nr<board.length && nc<board[0].length){
+                if(board[nr][nc]=='O'&& vis[nr][nc]==0){
+                    vis[nr][nc] = 1;
+                    dfs(board,vis,nr,nc);
                 }
             }
         }
@@ -36,36 +21,21 @@ class Solution {
 
         int[][] vis = new int[n][m];
 
-        //Applying bfs/dfs for all the boundaries
-
-        for (int i = 0; i < n; i++) {
-            if (vis[i][0] == 0 && board[i][0] == 'O') {
-                bfs(i, 0,board,vis);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    if(vis[i][j]==0 && board[i][j]=='O'){
+                        vis[i][j]=1;
+                        dfs(board,vis,i,j);
+                    }
+                }
             }
         }
 
-        for (int j = 0; j < m; j++) {
-            if (vis[0][j] == 0 && board[0][j] == 'O') {
-                bfs(0, j,board,vis);
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (vis[i][m - 1] == 0 && board[i][m - 1] == 'O') {
-                bfs(i, m - 1,board,vis);
-            }
-        }
-
-        for (int j = 0; j < m; j++) {
-            if (vis[n - 1][j] == 0 && board[n - 1][j] == 'O') {
-                bfs(n - 1, j,board,vis);
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (vis[i][j] == 0 && board[i][j] == 'O') {
-                    board[i][j] = 'X';
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O' && vis[i][j]==0){
+                    board[i][j]='X';
                 }
             }
         }
