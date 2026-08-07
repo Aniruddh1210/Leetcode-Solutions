7bed75c4563283c902be77b3aea964aea75b0e10
Ani <111388269+Aniruddh1210@users.noreply.github.com>
2026-08-08 02:03:06 +0530

    Time: 0 ms (100%), Space: 52.4 MB (97.62%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,12 +1,15 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int left = 0, right = nums.length-1;
-        while(left < right){
+        int left = 1, right = nums.length-2;
+        if(nums.length==1)return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[nums.length-1]!=nums[nums.length-2]) return nums[nums.length-1];
+        while(left <= right){
             int mid = (left + right)/2;
             if( (mid % 2 == 0 && nums[mid] == nums[mid +1]) || (mid %2 == 1 && nums[mid] == nums[mid - 1]) )
                 left = mid + 1;
             else
-                right = mid;
+                right = mid - 1;
         }
         return nums[left];
     }
